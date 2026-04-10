b3b7da75685e81642c03c11bc5a9d3c430bfb06d
Scott K Logan <logans@cottsay.net>
2026-04-10 14:39:00 -0500

    Add typing information to Python extension

--- test/test_smoke.py
+++ test/test_smoke.py
@@ -3,14 +3,15 @@
 
 from pathlib import Path
 import shutil
+from typing import Generator
 
 import createrepo_agent
 import pytest
 
-FIXTURES_DIR = Path(__file__).parent / 'fixtures'
-EMPTY_REPO = FIXTURES_DIR / 'empty'
-POPULATED_REPO = FIXTURES_DIR / 'populated'
-POPULATED_RPM = Path(
+FIXTURES_DIR: Path = Path(__file__).parent / 'fixtures'
+EMPTY_REPO: Path = FIXTURES_DIR / 'empty'
+POPULATED_REPO: Path = FIXTURES_DIR / 'populated'
+POPULATED_RPM: Path = Path(
     POPULATED_REPO,
     'x86_64',
     'Packages',
@@ -20,7 +21,7 @@ POPULATED_RPM = Path(
 
 
 @pytest.fixture
-def mutable_populated_repo(tmp_path):
+def mutable_populated_repo(tmp_path: Path) -> Generator[Path, None, None]:
     repo_path = tmp_path / 'populated'
     shutil.copytree(
         str(POPULATED_REPO), str(repo_path),
@@ -28,21 +29,21 @@ def mutable_populated_repo(tmp_path):
     yield repo_path
 
 
-def test_version():
+def test_version() -> None:
     assert createrepo_agent.__version__
 
 
-def test_add(tmp_path):
+def test_add(tmp_path: Path) -> None:
     rpm_path = str(POPULATED_RPM)
 
     with createrepo_agent.Server(str(tmp_path)):
         with createrepo_agent.Client(str(tmp_path)) as c:
             with pytest.raises(TypeError):
-                c.add(None)
+                c.add(None)  # type: ignore[arg-type]
             with pytest.raises(TypeError):
-                c.add(rpm_path, 1)
+                c.add(rpm_path, 1)  # type: ignore[arg-type]
             with pytest.raises(TypeError):
-                c.add(rpm_path, (1,))
+                c.add(rpm_path, (1,))  # type: ignore[arg-type]
             c.set_invalidate_dependants(True)
             c.set_invalidate_family(True)
             c.add(rpm_path, ('x86_64',))
@@ -55,13 +56,13 @@ def test_add(tmp_path):
     assert (arch_path / 'Packages' / 'r' / POPULATED_RPM.name).is_file()
 
 
-def test_commit_nothing(tmp_path):
+def test_commit_nothing(tmp_path: Path) -> None:
     with createrepo_agent.Server(str(tmp_path)):
         with createrepo_agent.Client(str(tmp_path)) as c:
             c.commit()
 
 
-def test_server_socket_collision(tmp_path):
+def test_server_socket_collision(tmp_path: Path) -> None:
     with createrepo_agent.Server(str(tmp_path)):
         pass
 
@@ -71,16 +72,16 @@ def test_server_socket_collision(tmp_path):
                 pass
 
 
-def test_sync_all(tmp_path):
+def test_sync_all(tmp_path: Path) -> None:
     base_url = POPULATED_REPO.as_uri()
     with createrepo_agent.Server(str(tmp_path)):
         with createrepo_agent.Client(str(tmp_path)) as c:
             with pytest.raises(TypeError):
-                c.sync(None)
+                c.sync(None)  # type: ignore[arg-type]
             with pytest.raises(TypeError):
-                c.sync(base_url, arches=1)
+                c.sync(base_url, arches=1)  # type: ignore[arg-type]
             with pytest.raises(TypeError):
-                c.sync(base_url, arches=(1,))
+                c.sync(base_url, arches=(1,))  # type: ignore[arg-type]
             c.sync(base_url, arches=('x86_64',))
             c.commit()
 
@@ -103,7 +104,7 @@ def test_sync_all(tmp_path):
     assert old_repomd_contents == repomd_path.read_text()
 
 
-def test_sync_pattern_hit(tmp_path):
+def test_sync_pattern_hit(tmp_path: Path) -> None:
     base_url = POPULATED_REPO.as_uri()
     pattern = POPULATED_RPM.name[:3] + '.*'
     with createrepo_agent.Server(str(tmp_path)):
@@ -130,7 +131,7 @@ def test_sync_pattern_hit(tmp_path):
     assert not (arch_path / 'Packages' / 'r' / POPULATED_RPM.name).is_file()
 
 
-def test_sync_pattern_miss(tmp_path):
+def test_sync_pattern_miss(tmp_path: Path) -> None:
     base_url = POPULATED_REPO.as_uri()
     pattern = 'does-not-match'
     with createrepo_agent.Server(str(tmp_path)):
@@ -143,16 +144,16 @@ def test_sync_pattern_miss(tmp_path):
     assert not (arch_path / 'Packages' / 'r' / POPULATED_RPM.name).is_file()
 
 
-def test_remove_name(mutable_populated_repo):
+def test_remove_name(mutable_populated_repo: Path) -> None:
     arches = ('x86_64', )
     with createrepo_agent.Server(str(mutable_populated_repo)):
         with createrepo_agent.Client(str(mutable_populated_repo)) as c:
             with pytest.raises(TypeError):
-                c.remove_name(1)
+                c.remove_name(1)  # type: ignore[arg-type]
             with pytest.raises(TypeError):
-                c.remove_name('ros-dev-tools', (1,))
+                c.remove_name('ros-dev-tools', (1,))  # type: ignore[arg-type]
             with pytest.raises(TypeError):
-                c.remove_name('ros-dev-tools', arches, 1)
+                c.remove_name('ros-dev-tools', arches, 1)  # type: ignore[call-arg]
 
             # Omitting arches targets SRPMS which has no such package
             c.remove_name('ros-dev-tools')
@@ -190,16 +191,16 @@ def test_remove_name(mutable_populated_repo):
         assert not pkg_path.is_file()
 
 
-def test_remove_pattern(mutable_populated_repo):
+def test_remove_pattern(mutable_populated_repo: Path) -> None:
     arches = ('x86_64', )
     with createrepo_agent.Server(str(mutable_populated_repo)):
         with createrepo_agent.Client(str(mutable_populated_repo)) as c:
             with pytest.raises(TypeError):
-                c.remove_pattern(1)
+                c.remove_pattern(1)  # type: ignore[arg-type]
             with pytest.raises(TypeError):
-                c.remove_pattern('ros-.*', (1,))
+                c.remove_pattern('ros-.*', (1,))  # type: ignore[arg-type]
             with pytest.raises(TypeError):
-                c.remove_pattern('ros-.*', arches, 1)
+                c.remove_pattern('ros-.*', arches, 1)  # type: ignore[call-arg]
 
             # Omitting arches targets SRPMS which has no such package
             c.remove_pattern('ros-.*')
@@ -242,7 +243,7 @@ def test_remove_pattern(mutable_populated_repo):
     'invalidate_family',
     'missing_ok',
 ))
-def test_option_arguments(tmp_path, option_name):
+def test_option_arguments(tmp_path: Path, option_name: str) -> None:
     with createrepo_agent.Server(str(tmp_path)):
         with createrepo_agent.Client(str(tmp_path)) as c:
             setter = getattr(c, f'set_{option_name}')
